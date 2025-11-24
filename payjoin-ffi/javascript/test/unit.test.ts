025fec122c925d6d76ee76501bca22d274645bb6
chavic <dev.chavic@gmail.com>
2025-11-24 13:42:49 +0200

    test(js): add validation coverage for primitive API

--- payjoin-ffi/javascript/test/unit.test.ts
+++ payjoin-ffi/javascript/test/unit.test.ts
@@ -169,3 +169,45 @@ describe("Persistence tests", () => {
         assert.ok(withReplyKey, "Sender should be created successfully");
     });
 });
+
+describe("Validation", () => {
+    test("receiver builder rejects bad address", () => {
+        assert.throws(() => {
+            new payjoin.ReceiverBuilder(
+                "not-an-address",
+                "https://example.com",
+                payjoin.OhttpKeys.decode(
+                    new Uint8Array([
+                        0x01, 0x00, 0x16, 0x04, 0xba, 0x48, 0xc4, 0x9c, 0x3d, 0x4a, 0x92,
+                        0xa3, 0xad, 0x00, 0xec, 0xc6, 0x3a, 0x02, 0x4d, 0xa1, 0x0c, 0xed,
+                        0x02, 0x18, 0x0c, 0x73, 0xec, 0x12, 0xd8, 0xa7, 0xad, 0x2c, 0xc9,
+                        0x1b, 0xb4, 0x83, 0x82, 0x4f, 0xe2, 0xbe, 0xe8, 0xd2, 0x8b, 0xfe,
+                        0x2e, 0xb2, 0xfc, 0x64, 0x53, 0xbc, 0x4d, 0x31, 0xcd, 0x85, 0x1e,
+                        0x8a, 0x65, 0x40, 0xe8, 0x6c, 0x53, 0x82, 0xaf, 0x58, 0x8d, 0x37,
+                        0x09, 0x57, 0x00, 0x04, 0x00, 0x01, 0x00, 0x03,
+                    ]).buffer,
+                ),
+            );
+        });
+    });
+
+    test("input pair rejects invalid outpoint", () => {
+        assert.throws(() => {
+            const txin = payjoin.PlainTxIn.create({
+                previousOutput: payjoin.PlainOutPoint.create({
+                    txid: "deadbeef",
+                    vout: 0,
+                }),
+                scriptSig: new Uint8Array([]),
+                sequence: 0,
+                witness: [],
+            });
+            const psbtIn = payjoin.PlainPsbtInput.create({
+                witnessUtxo: undefined,
+                redeemScript: undefined,
+                witnessScript: undefined,
+            });
+            new payjoin.InputPair(txin, psbtIn, undefined);
+        });
+    });
+});
